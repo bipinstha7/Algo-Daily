7ed89f9ea17af1f81bb276a28b7c32eace809bfd
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-17 18:33:50 +0545

    reverse string multiple methods

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,8 +6,42 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// method 1
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// method 2
 function reverse(str) {
-  return str.split('').reverse().join('');
+  let newStr = [];
+  str.split('').map(char => {
+    return newStr.unshift(char);
+  });
+  return newStr.join('');
 }
+
+// method 3
+// function reverse(str) {
+//   let newStr = '';
+//   str.split('').map(char => {
+//     newStr = char + newStr;
+//   });
+//   return newStr;
+// }
+
+// method 4
+// function reverse(str) {
+//   let newStr = '';
+//   for(let char of str) {
+//     newStr = char + newStr;
+//   }
+//   return newStr;
+// }
+
+// method 5
+// function reverse(str) {
+//   return str.split('').reduce((rev, char) => char + rev, '');
+// }
+
 
 module.exports = reverse;
