feec5a1d005d422c1870fe56ba64f53a7001c009
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-18 12:12:07 +0545

    return a new array of multiple of numbers other than itself

---
+++ DCP/return a new array.js
@@ -0,0 +1,23 @@
+// Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+// For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+
+
+const multipleOfArray = (arr) => {
+  let mul = 1;
+  for(let i=0; i<arr.length; i++) {
+    mul *= arr[i];
+  }
+  let newArray = [];
+  for(let j=0; j<arr.length; j++) {
+    let number = mul / arr[j];
+    newArray.push(number);
+  }
+  console.log("mul:", mul);
+  console.log("new array:", newArray);
+}
+
+const array = [1,2,3,4,5];
+// const array = [3, 2, 1];
+multipleOfArray(array);
