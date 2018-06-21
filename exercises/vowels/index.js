0c4543905df729a61763ea34116a0c4633a499db
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-21 14:03:17 +0545

    count no.of vowels in a string/sentence

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,37 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// method 1
+// function vowels(str) {
+//   let count = 0;
+//   str.split('').map(char => {
+//     if(char === 'a') {
+//       count++;
+//     } else if(char === 'e') {
+//       count++;
+//     } else if(char === 'i') {
+//       count++;
+//     } else if(char === 'o') {
+//       count++;
+//     } else if(char === 'u') {
+//       count++
+//     }
+//   });
+//   return count;
+// }
+
+// method 2
+function vowels(str) {
+  let count = 0;
+  const checkVowels = 'aeiou';
+  for(let char of str.toLowerCase()) {
+    if(checkVowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+
 
 module.exports = vowels;
