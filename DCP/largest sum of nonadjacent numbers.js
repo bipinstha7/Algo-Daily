75efd14c7a3846cc1623a5532edf2b709646715e
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-27 10:25:44 +0545

    find largest sum of non-adjacent numbers

---
+++ DCP/largest sum of nonadjacent numbers.js
@@ -0,0 +1,19 @@
+// Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
+
+// For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+
+const largestSumOfNonadjacentNumbers = (arr) => {
+  let include = arr[0];
+  let exclude = 0;
+  
+  for(let i = 1; i < arr.length; i++) {
+    let sum = include > exclude ? include : exclude;
+    include = exclude + arr[i];
+    exclude = sum;
+  }
+  console.log(include > exclude ? include : exclude);
+}
+
+// const array = [2, 4, 6, 2, 5];
+const array = [5, 1, 1, 5];
+largestSumOfNonadjacentNumbers(array);
