1f3ad0b2097a74cd8ada2abf4715a57253b35615
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-29 07:54:39 +0545

    rename and refactor to test

--- DCP/Algorithms/2.largestsumofnonadjacentnumbers.js
+++ DCP/Algorithms/2.largestsumofnonadjacentnumbers.js
@@ -2,6 +2,25 @@
 
 // For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
 
+// const largestSumOfNonadjacentNumbers = (arr) => {
+//   let include = arr[0];
+//   let exclude = 0;
+  
+//   for(let i = 1; i < arr.length; i++) {
+//     let sum = include > exclude ? include : exclude;
+//     include = exclude + arr[i];
+//     exclude = sum;
+//   }
+//   return (include > exclude ? include : exclude);
+// }
+
+// // const array = [2, 4, 6, 2, 5];
+// const array = [5, 1, 1, 5];
+// const result = largestSumOfNonadjacentNumbers(array);
+// console.log(result);
+
+
+// FOR TEST WITH JEST
 const largestSumOfNonadjacentNumbers = (arr) => {
   let include = arr[0];
   let exclude = 0;
@@ -12,9 +31,7 @@ const largestSumOfNonadjacentNumbers = (arr) => {
     exclude = sum;
   }
   return (include > exclude ? include : exclude);
-}
+};
 
-// const array = [2, 4, 6, 2, 5];
-const array = [5, 1, 1, 5];
-const result = largestSumOfNonadjacentNumbers(array);
-console.log(result);
+module.exports = {largestsum: largestSumOfNonadjacentNumbers};
+
