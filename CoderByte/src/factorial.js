b30bcfb965d8688014c374ba7449d9c16bd68ac6
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-03 22:36:32 +0545

    factorial function with recursion method

--- CoderByte/src/factorial.js
+++ CoderByte/src/factorial.js
@@ -5,14 +5,20 @@
  * For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24.
  */
 
- // METHOD 1
- function firstFactorial(num) {
-    let factorial = 1
-    for (let i = 1; i <= num; i++) {
-        factorial *= i
-    }
+// METHOD 1
+function firstFactorial(num) {
+	let factorial = 1
+	for (let i = 1; i <= num; i++) {
+		factorial *= i
+	}
 
-    return factorial
- }
+	return factorial
+}
 
- module.exports = firstFactorial
+//  METHOD 2
+function firstFactorial2(num) {
+	if (num === 0 || num === 1) return 1
+	return num * firstFactorial2(num - 1)
+}
+
+module.exports = { firstFactorial, firstFactorial2 }
