4510de84fc24d78519ed18c36ce3879cec949b4f
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-03 22:35:30 +0545

    test case for both of the methods

--- CoderByte/tests/factorial.test.js
+++ CoderByte/tests/factorial.test.js
@@ -1,8 +1,13 @@
-const { firstFactorial } = require('../src/factorial')
+const { firstFactorial, firstFactorial2 } = require('../src/factorial')
 
 describe('firstFactorial', () => {
-	it('should return factorial of a given number', () => {
-		const result = firstFactorial(4)
-		expect(result).toBe(24)
+	it('should return factorial of a given number: METHOD 1', () => {
+        const result = firstFactorial(4)
+        expect(result).toBe(24)
+    })
+    
+    it('should return factorial of a given number: METHOD 2', () => {
+        const result2 = firstFactorial2(4)
+        expect(result2).toBe(24)
 	})
 })
