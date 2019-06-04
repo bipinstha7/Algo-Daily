df89920f7f7a6025952ed83b43e21a7a803ca569
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-04 07:52:59 +0545

    change function name from firstFactorial to factorial

--- CoderByte/src/factorial.js
+++ CoderByte/src/factorial.js
@@ -1,12 +1,12 @@
 /**
- * Have the function FirstFactorial(num)
+ * Have the function factorial(num)
  * take the num parameter being passed and
  * return the factorial of it.
  * For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24.
  */
 
 // METHOD 1
-function firstFactorial(num) {
+function factorial(num) {
 	let factorial = 1
 	for (let i = 1; i <= num; i++) {
 		factorial *= i
@@ -16,9 +16,9 @@ function firstFactorial(num) {
 }
 
 //  METHOD 2
-function firstFactorial2(num) {
+function factorial2(num) {
 	if (num === 0 || num === 1) return 1
-	return num * firstFactorial2(num - 1)
+	return num * factorial2(num - 1)
 }
 
-module.exports = { firstFactorial, firstFactorial2 }
+module.exports = { factorial, factorial2 }
--- CoderByte/tests/factorial.test.js
+++ CoderByte/tests/factorial.test.js
@@ -1,13 +1,13 @@
-const { firstFactorial, firstFactorial2 } = require('../src/factorial')
+const { factorial, factorial2 } = require('../src/factorial')
 
 describe('firstFactorial', () => {
 	it('should return factorial of a given number: METHOD 1', () => {
-        const result = firstFactorial(4)
+        const result = factorial(4)
         expect(result).toBe(24)
     })
     
     it('should return factorial of a given number: METHOD 2', () => {
-        const result2 = firstFactorial2(4)
+        const result2 = factorial2(4)
         expect(result2).toBe(24)
 	})
 })
