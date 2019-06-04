69adc3073626e7e18f0b75140fc7a1e714b9d370
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-04 08:16:29 +0545

    reverse string module

--- CoderByte/src/reverseString.js
+++ CoderByte/src/reverseString.js
@@ -1,7 +1,13 @@
 /**
- * Have the function FirstReverse(str) 
+ * Have the function reverseString(str) 
  * ake the str parameter being passed and 
  * return the string in reversed order. 
  * For example: if the input string is "Hello World and Coders" 
  * then your program should return the string sredoC dna dlroW olleH.
  */
+
+ function reverseString(str) {
+    return str.split('').reverse().join('')
+ }
+
+module.exports = reverseString
