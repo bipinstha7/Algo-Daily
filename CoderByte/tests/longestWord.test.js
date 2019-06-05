340cb107d585edea991df7ee5c94a39c7deffab5
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-05 08:10:10 +0545

    test longestword with punctuation

--- CoderByte/tests/longestWord.test.js
+++ CoderByte/tests/longestWord.test.js
@@ -2,7 +2,7 @@ const longestWord = require('../src/longestWord')
 
 describe('Longest word from a sentence', () => {
 	it('should return a longest word from a given sentence', () => {
-		const result = longestWord('I love dogs')
-		expect(result).toEqual('love')
+		const result = longestWord('fun&!! time')
+		expect(result).toEqual('time')
 	})
 })
