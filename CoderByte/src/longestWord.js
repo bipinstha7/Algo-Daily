60d9ca4c63e37b1276d832b8346b664481780794
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-08 21:37:19 +0545

    Remove console message

--- CoderByte/src/longestWord.js
+++ CoderByte/src/longestWord.js
@@ -1,27 +1,26 @@
 /**
- * Have the function longestWord(sen) 
- * take the sen parameter being passed and 
- * return the largest word in the string. 
- * If there are two or more words that are the same length, 
- * return the first word from the string with that length. 
- * Ignore punctuation and assume sen will not be empty. 
- * 
+ * Have the function longestWord(sen)
+ * take the sen parameter being passed and
+ * return the largest word in the string.
+ * If there are two or more words that are the same length,
+ * return the first word from the string with that length.
+ * Ignore punctuation and assume sen will not be empty.
+ *
  * Input:"fun&!! time"
  * Output:"time"
- * 
+ *
  * Input:"I love dogs"
  * Output:"love"
  */
 
- function longestWord(sen) {
-    const senWithoutPunctuation = sen.replace(/[^a-zA-Zsd]/g, ' ')
-    console.log(senWithoutPunctuation)
-    const senInArray = senWithoutPunctuation.split(' ')
-    const lengths = senInArray.map(arr => arr.length)
-    const maxLength = Math.max(...lengths)
-    const result = senInArray.filter(arr => arr.length === maxLength)
-    
-    return result[0]
- }
+function longestWord(sen) {
+	const senWithoutPunctuation = sen.replace(/[^a-zA-Zsd]/g, ' ')
+	const senInArray = senWithoutPunctuation.split(' ')
+	const lengths = senInArray.map(arr => arr.length)
+	const maxLength = Math.max(...lengths)
+	const result = senInArray.filter(arr => arr.length === maxLength)
+
+	return result[0]
+}
 
 module.exports = longestWord
