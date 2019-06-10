c8eb607d7ee7ab32ff90891b2973b21e7de7be82
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-10 21:42:03 +0545

    Convert given time in seconds in hour:minutes format

---
+++ CoderByte/src/timeConvert.js
@@ -0,0 +1,25 @@
+/**
+ * Have the function timeConvert(num) take the num parameter being passed
+ * and return the number of hours and minutes the parameter converts to
+ * (ie. if num = 63 then the output should be 1:3).
+ * Separate the number of hours and minutes with a colon.
+ *
+ * Input:126
+ * Output:"2:6"
+ *
+ * Input:45
+ * Output:"0:45"
+ */
+
+function timeConvert(num) {
+	if (num < 60) {
+		return `0:${num}`
+	}
+
+	const hour = Math.floor(num / 60)
+	const minutes = num % 60
+
+	return `${hour}:${minutes}`
+}
+
+module.exports = timeConvert
