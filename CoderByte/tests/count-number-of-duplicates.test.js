b94f3eb37cd1b05064d462aa0282884cef9a103f
bipinstha7 <bipin.shrestha901@gmail.com>
2019-06-22 19:11:48 +0545

    Fix number typo

--- CoderByte/tests/count-number-of-duplicates.test.js
+++ CoderByte/tests/count-number-of-duplicates.test.js
@@ -1,38 +1,38 @@
-const countDuplicates = require('../src/count-numbet-of-duplicates')
+const countDuplicates = require('../src/count-number-of-duplicates')
 
 describe('count number of duplicates', () => {
 	it('should return 0', () => {
-        const result = countDuplicates("abcde")
-        expect(result).toBe(0)
-    })
+		const result = countDuplicates('abcde')
+		expect(result).toBe(0)
+	})
 
-    it('should return 2', () => {
-        const result = countDuplicates("aabbcde")
-        expect(result).toBe(2)
-    })
+	it('should return 2', () => {
+		const result = countDuplicates('aabbcde')
+		expect(result).toBe(2)
+	})
 
-    it('should return 2', () => {
-        const result = countDuplicates("aabBcde")
-        expect(result).toBe(2)
-    })
+	it('should return 2', () => {
+		const result = countDuplicates('aabBcde')
+		expect(result).toBe(2)
+	})
 
-    it('should return 1', () => {
-        const result = countDuplicates("indivisibility")
-        expect(result).toBe(1)
-    })
+	it('should return 1', () => {
+		const result = countDuplicates('indivisibility')
+		expect(result).toBe(1)
+	})
 
-    it('should return 2', () => {
-        const result = countDuplicates("Indivisibilities")
-        expect(result).toBe(2)
-    })
+	it('should return 2', () => {
+		const result = countDuplicates('Indivisibilities')
+		expect(result).toBe(2)
+	})
 
-    it('should return 2', () => {
-        const result = countDuplicates("aA11")
-        expect(result).toBe(2)
-    })
+	it('should return 2', () => {
+		const result = countDuplicates('aA11')
+		expect(result).toBe(2)
+	})
 
-    it('should return 2', () => {
-        const result = countDuplicates("ABBA")
-        expect(result).toBe(2)
-    })
+	it('should return 2', () => {
+		const result = countDuplicates('ABBA')
+		expect(result).toBe(2)
+	})
 })
